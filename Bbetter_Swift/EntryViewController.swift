276a334383351da4ed25ef6ff5fb45c1a10df31f
son <sangbae6840@gmail.com>
2022-05-15 23:06:39 +0900

    TableView 추가 및 설정

---
+++ Bbetter_Swift/EntryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  EntryViewController.swift
+//  Bbetter_Swift
+//
+//  Created by 모바일인터넷과 on 2022/05/15.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
---
+++ Bbetter_Swift/GoalController.swift
@@ -0,0 +1,62 @@
+//
+//  GoalController.swift
+//  Bbetter_Swift
+//
+//  Created by 모바일인터넷과 on 2022/05/15.
+//
+
+import UIKit
+import RealmSwift
+
+class ToDoListItem: Object {
+    @objc dynamic var item: String = ""
+    @objc dynamic var date: Date = Date()
+
+}
+
+class GoalController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    
+    
+    @IBOutlet var table: UITableView!
+    private var data = [ToDoListItem]()
+    private let realm = try! Realm()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        data = realm.objects(ToDoListItem.self).map({ $0 })
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        self.table.delegate = self
+        self.table.dataSource = self
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row].item
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+    }
+    
+    @IBAction func didTapAddButton(){
+        guard let vc = storyboard?.instantiateViewController(identifier: "enter") as? EntryViewController else {
+            return
+        }
+        vc.completionHandler = { [weak self] in
+            self?.refresh()
+            
+            
+        }
+
+}
