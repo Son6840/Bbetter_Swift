263321f7f7af0de4fff5eae29f7f1ae9eb705fb1
son <sangbae6840@gmail.com>
2022-05-29 23:17:55 +0900

    TabMan

---
+++ Bbetter_Swift/newCulture_ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  newCultureViewController.swift
+//  Bbetter_Swift
+//
+//  Created by 모바일인터넷과 on 2022/05/29.
+//
+
+import UIKit
+
+class newCultureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+
+   
+    
+}
---
+++ Bbetter_Swift/newsIT_ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  newsIT_ViewController.swift
+//  Bbetter_Swift
+//
+//  Created by 모바일인터넷과 on 2022/05/29.
+//
+
+import UIKit
+import SwiftSoup
+
+class newsIT_ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+ 
+    private var newsTitle:Elements!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+
+  
+
+}
